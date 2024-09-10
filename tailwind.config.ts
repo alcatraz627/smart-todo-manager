646fd7b66e1a98fd8a263eb90e4eba2b59b53169
Aakarsh Chopra <alcatrazz.627@gmail.com>
2024-09-11 01:22:20 +0530

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
+import daisyui from "npm:daisyui";
 import { type Config } from "tailwindcss";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["cupcake"],
+  },
 } satisfies Config;
