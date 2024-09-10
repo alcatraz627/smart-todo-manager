f380c4e5f6f3c997a81204d849abce88c738d673
alcatraz627 <alcatrazz.627@gmail.com>
2024-09-10 20:08:47 +0530

    add env to actions file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,15 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import { type Manifest } from "$fresh/server.ts";
-import * as $Container from "./islands/Container.tsx";
-import * as $Countdown from "./islands/Countdown.tsx";
-import * as $Counter from "./islands/Counter.tsx";
-import * as $chat_ChatWidget from "./islands/chat/ChatWidget.tsx";
-import * as $chat_RichText from "./islands/chat/RichText.tsx";
-import * as $todo_NewTodo from "./islands/todo/NewTodo.tsx";
-import * as $todo_TodoList from "./islands/todo/TodoList.tsx";
-import * as $todo_TodoRow from "./islands/todo/TodoRow.tsx";
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $ai_index from "./routes/ai/index.ts";
@@ -20,6 +11,15 @@ import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $todos_index from "./routes/todos/index.tsx";
+import * as $Container from "./islands/Container.tsx";
+import * as $Countdown from "./islands/Countdown.tsx";
+import * as $Counter from "./islands/Counter.tsx";
+import * as $chat_ChatWidget from "./islands/chat/ChatWidget.tsx";
+import * as $chat_RichText from "./islands/chat/RichText.tsx";
+import * as $todo_NewTodo from "./islands/todo/NewTodo.tsx";
+import * as $todo_TodoList from "./islands/todo/TodoList.tsx";
+import * as $todo_TodoRow from "./islands/todo/TodoRow.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
